5631052df9b411000199c7c317e330aaa493c001
leonelfer99 <leo99.fernandes@gmail.com>
2022-03-11 22:57:16 -0300

    agregue herencia de templates y admin

--- indice/urls.py
+++ indice/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import inicio, otra_vista, numero_random, numero_usuario, cumpleaños, mi_plantilla
 
 urlpatterns = [
-    path('', inicio),
-    path('otra_vista', otra_vista),
-    path('numero-random/', numero_random),
-    path('dame-random/<int:numero>', numero_usuario),
-    path('edad/<int:año>', cumpleaños),
-    path('mi_plantilla/', mi_plantilla),
+    path('', inicio, name="inicio"),
+    path('otra_vista', otra_vista, name="otra_vista"),
+    path('numero-random/', numero_random, name="numero-random"),
+    path('dame-random/<int:numero>', numero_usuario, name="dame-random"),
+    path('edad/<int:año>', cumpleaños, name="cumpleaños"),
+    path('mi_plantilla/', mi_plantilla, name="mi_plantilla"),
 ]
